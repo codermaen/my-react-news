58da90206c27b2df1a6cb7d4e28746ddd8ca9988
Arun <codermaen@gmail.com>
2021-11-11 18:33:05 +0530

    initial load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Link, BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
+import countries from './countries.json'
 
-function App() {
+const CountriesPage = () => {
+
+  const { country } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${'45512bf52fe44ac888e6bfb092b69da0'}&pageSize=5`, {})
+      .then((res) => res.json())
+      .then((response) => {
+        setData(response.articles);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error, 'custom'));
+  }, [country]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      {!isLoading && data.map((news, index) => {
+        return (
+          <div key={index} style={{ padding: 20, display: 'flex', flexDirection: 'row' }}>
+            <img
+              src={news.urlToImage}
+              height={108}
+              width={108}
+              alt={'news'}
+            />
+            <div style={{ padding: 10, marginTop: -40 }}>
+              <a href={news.url}><h1>{news.title}</h1></a>
+              {news.description}
+              <br />
+              {news.publishedAt}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const HomePage = () => {
+  return (
+
+    <>
+      <h3 style={{ alignSelf: 'center' }}>News</h3>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-between' }}>
+        {countries.map((country, index) => {
+          return (
+            <div key={index}>
+              <div style={{ padding: 10 }}>
+                <Link to={`/country/${country}`}>{country}</Link>
+
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+
+};
+const App = () => {
+  return (
+    <div >
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/country/:country" element={<CountriesPage />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
